9bfc3e625c8fb74a246b4b3d81e8c96c146e255a
itaasa <itaasarvin@gmail.com>
2022-08-01 19:14:56 -0400

    added service

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import * as config from 'config';
 import { AppModule } from './app.module';
-import { DbConnection } from './db';
-import { CarSchedule } from './interfaces/car-schedule.interface';
 import { Car } from './interfaces/car.interface';
-import { MaintenanceData } from './interfaces/maintenance-data.interface';
-import { mapDataToCarSchedule } from './mappings/car-schedule-mapper';
-import * as maintenanceJson from './subaru-brz-maintenance.json';
+import { CarScheduleService } from './car-schedule/car-schedule.service';
 
 start();
 test();
@@ -17,30 +12,14 @@ async function start() {
 }
 
 async function test() {
-  // const app = await NestFactory.create(AppModule);
-  // await app.listen(3000);
-
   const subaruBrz: Car = {
     make: 'Subaru',
     model: 'BRZ',
     year: 2014,
   };
 
-  const subaruBrzMaintenance: MaintenanceData = <MaintenanceData>(
-    maintenanceJson
-  );
+  const carScheduleService = new CarScheduleService();
 
-  const carSchedule: CarSchedule = await mapDataToCarSchedule(
-    subaruBrz,
-    subaruBrzMaintenance,
-  );
-
-  try {
-    let db = await new DbConnection().get();
-    await db
-      .collection(config.get('mongoCarScheduleCollection'))
-      .insertOne(carSchedule);
-  } catch (e) {
-    throw e;
-  }
+  const carSchedule = await carScheduleService.getCarSchedule(subaruBrz);
+  console.log(carSchedule);
 }
