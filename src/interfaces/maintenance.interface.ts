73a7d52848d7d022776ea6db58c283ef00f5f45f
itaasa <itaasarvin@gmail.com>
2022-08-15 19:29:42 -0400

    added history to maintenance

--- src/interfaces/maintenance.interface.ts
+++ src/interfaces/maintenance.interface.ts
@@ -1,7 +1,8 @@
+import { History } from './history.interface';
+
 export interface Maintenance {
   description: string;
   cost: number;
   frequency: number;
-  lastCompletedDate: Date;
-  completedMileage: number;
+  history: History[];
 }
